8301e8e38042b8f25d17c431ff6787603bef0c22
littleBitsman <67122660+littleBitsman@users.noreply.github.com>
2025-10-06 15:54:09 -0400

    o

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     let _l = std::io::stdout().lock();
 
-    luau0_src::Build::new()
+    luau_build::Build::new()
         .enable_codegen(false)
         .set_max_cstack_size(1000000)
         .set_vector_size(3)
